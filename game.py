0e6f6f7f34cd84e60f11fb56a0d8922d129b08f2
99061142 <99061142@mydavinci.nl>
2022-01-15 10:51:41 +0100

    Made an function to make the inputs

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ window = tk.Tk() # Make the window
 
 
 # Array for the questions, and the information about the fails
-question_information = {
+questions_information = {
     "easy": {
         "questions": [
             "1 + 1",
@@ -51,39 +51,72 @@ question_information = {
 
 
 chosen_difficulty = tk.StringVar(value="easy")
+question_answers = []
 
 
-# Make the question
+# Clear the window
+def clear_window():
+    for item in window.winfo_children():
+        item.destroy()
+
+
+# Make the head label
 def make_label(text):
-    tk.Label(text=text, font=('arial', 14)).grid(row=0, pady=('0', '10')) # Make the question and add it to the window    
+    tk.Label(text=text, font=('arial', 14)).grid(row=0, pady=('0', '10'), columnspan=2) # Make the question and add it to the window    
 
 
 # Make the submit button
-def make_submit():  
-    tk.Button(text='submit', bg='gray', font=('arial', 10), command=show_questions).grid(sticky='EW', pady=('10', '0'))
+def make_submit(command):  
+    tk.Button(text='submit', bg='gray', font=('arial', 10), command=command).grid(sticky='EW', columnspan=2, pady=('10', '0'))
+
+
+# Make the input(s)
+def make_input(input_type:str, array):
+    # Check if the given array is a function that must be called
+    try:
+        array = array()
+    
+    # If it is not a function that must be called
+    except TypeError:
+        pass
+
+
+    if input_type == "spinbox":
+        for row, question in enumerate(array):
+            question_row = row + 1 # Row of the question label + input
+
+            # Add all the answers to a list
+            answer = tk.StringVar()
+            question_answers.append(answer)
+
+
+            tk.Label(text=question, font=('arial', 14)).grid(row=question_row, column=0, pady=('0', '10')) # Input label
+            tk.Spinbox(window, textvariable=answer, from_=0, to=float('inf')).grid(row=question_row, column=1) # Input
+
+    elif input_type == "radiobutton":
+        for row, difficulty_option in enumerate(array):
+            ttk.Radiobutton(window, text=difficulty_option, value=difficulty_option, variable=chosen_difficulty).grid(sticky='e') # Input
+
+
+# Return the questions of the difficulty the user is in
+def make_difficulty_questions():
+    questions = questions_information[chosen_difficulty.get()]['questions'] # ALl the questions for the difficulty the user is in
+
+    return questions # Return the questions
 
 
 def show_questions():   
-    questions = question_information[chosen_difficulty.get()]['questions']
-
-    print(questions)
-
-
-
-
-
-def gamemode_options():
-    for row, difficulty_option in enumerate(question_information):
-        question_row = row + 1
-
-        ttk.Radiobutton(window, text=difficulty_option, value=difficulty_option, variable=chosen_difficulty).grid(row=question_row) # Input
+    clear_window()
+    make_label(f"Calculate the questions on the screen")
+    make_input("spinbox", make_difficulty_questions)
+    make_submit("")
 
 
 
 def choose_gamemode():
     make_label("Choose the starting difficulty")
-    gamemode_options()
-    make_submit()
+    make_input("radiobutton", questions_information)
+    make_submit(show_questions)
 
 
 
