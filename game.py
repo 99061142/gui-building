0034a2cae1d4827b34942eae82fa45dec7822022
99061142 <99061142@mydavinci.nl>
2022-01-16 21:59:43 +0100

    Make the functions to get the first questions after the user chose the starting difficulty
    Added:
    
    Functions:
    
    * Make the label
    * Make the submit button
    * Add every question on the screen
    * Add every option on the screen for the starting difficulty

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ window = tk.Tk() # Make the window
 
 
 # Array for the questions, and the information about the fails
-questions_information = {
+sum_information = {
     "easy": {
         "questions": [
             "1 + 1",
@@ -56,13 +56,13 @@ questions_information = {
 }
 
 
-user_difficulty = tk.StringVar(value="easy") # WHich difficulty the user is on
-highest_difficulty = tk.StringVar()
-
-user_sum_answers = [] # All the sum answers of the user
+user_difficulty = tk.StringVar(value="easy") # Which difficulty the user is on
 
 
-# Clear the window
+sum_question_answer = [] # All the sum answers of the user
+
+
+# Clear everythign on the window
 def clear_window():
     for item in window.winfo_children():
         item.destroy()
@@ -70,13 +70,68 @@ def clear_window():
 
 # Make the head label
 def make_label(text:str):
-    tk.Label(text=text, font=('arial', 14)).grid(row=0, pady=('0', '10'), columnspan=2) # Make the question and add it to the window    
+    tk.Label(window, text=text, font=('arial', 14)).grid(row=0, pady=('0', '10')) # Label at the top
 
 
 # Make the submit button
 def make_submit(command):  
-    tk.Button(text='submit', bg='gray', font=('arial', 10), command=command).grid(sticky='EW', columnspan=2, pady=('10', '0'))
+    tk.Button(window, text='submit', bg='gray', font=('arial', 10), command=command).grid(sticky='EW', columnspan=2, pady=('10', '0')) # Submit button
 
 
-if __name__ == "__main__":
+# Validate the answers the user gave on the questions
+def validate_user_answer():
     pass
+
+
+
+# Make the questions / show the questions on screen
+def make_questions():
+    difficulty = user_difficulty.get() # Difficulty the user is on
+    questions = sum_information[difficulty]['questions'] # All the questions
+
+
+    # Loop through every question
+    for num, question in enumerate(questions):
+        row = num + 1
+
+        # Remember the answer of the user
+        answer = tk.StringVar()
+        sum_question_answer.append(answer)
+
+        tk.Label(window, text=f"{question} =", font=('arial', 14)).grid(row=row, column=0, pady=('0', '10')) # Show the question before the input
+        tk.Spinbox(window, textvariable=answer, from_=0, to=float('inf')).grid(row=row, column=1) # Input
+
+
+# Show all the difficulties the user can choose from
+def make_difficulty_options():
+    difficulties = list( sum_information.keys() ) # ALl the difficulties
+
+    # Let the user choose between all the difficulties
+    for num, difficulty in enumerate(difficulties):
+        row = num + 1
+
+        ttk.Radiobutton(window, text=f"{difficulty.capitalize()}", value=difficulty, variable=user_difficulty).grid(row=row)
+
+
+# Call the functions for the question screen
+def question_screen():  
+    clear_window() # Clear everything on the window
+
+    make_label(f"Questions for the difficulty '{user_difficulty.get()}'") # Label above the input(s)
+    make_questions() # Make / show all the questions on screen
+    make_submit(validate_user_answer) # Add the submit button
+
+
+# Call the functions for the starting difficulty choosing screen
+def starting_difficulty_screen():
+    make_label("Choose your starting difficulty") # Label above the input(s)
+    make_difficulty_options() # Make / show all the difficulties the user can choose to start 
+    make_submit(question_screen) # Add the submit button
+
+
+
+
+# If the program starts
+if __name__ == "__main__":
+    starting_difficulty_screen() # Function to choose the starting difficulty
+    window.mainloop()
